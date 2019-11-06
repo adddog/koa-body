a9ef5f557c22778d75394c82830bcebf53379034
adddog <32887897+adddog@users.noreply.github.com>
2019-11-05 16:47:25 -0800

    accept a form instance

--- index.js
+++ index.js
@@ -174,38 +174,45 @@ function formy(ctx, opts) {
   return new Promise(function (resolve, reject) {
     var fields = {};
     var files = {};
-    var form = new forms.IncomingForm(opts);
-    form.on('end', function () {
-      return resolve({
-        fields: fields,
-        files: files
+    var form;
+    if(typeof opts === 'object'){
+     form = new forms.IncomingForm(opts);
+      
+      form.on('end', function () {
+        return resolve({
+          fields: fields,
+          files: files
+        });
+      }).on('error', function (err) {
+        return reject(err);
+      }).on('field', function (field, value) {
+        if (fields[field]) {
+          if (Array.isArray(fields[field])) {
+            fields[field].push(value);
+          } else {
+            fields[field] = [fields[field], value];
+          }
+        } else {
+          fields[field] = value;
+        }
+      }).on('file', function (field, file) {
+        if (files[field]) {
+          if (Array.isArray(files[field])) {
+            files[field].push(file);
+          } else {
+            files[field] = [files[field], file];
+          }
+        } else {
+          files[field] = file;
+        }
       });
-    }).on('error', function (err) {
-      return reject(err);
-    }).on('field', function (field, value) {
-      if (fields[field]) {
-        if (Array.isArray(fields[field])) {
-          fields[field].push(value);
-        } else {
-          fields[field] = [fields[field], value];
-        }
-      } else {
-        fields[field] = value;
+      if (opts.onFileBegin) {
+        form.on('fileBegin', opts.onFileBegin);
       }
-    }).on('file', function (field, file) {
-      if (files[field]) {
-        if (Array.isArray(files[field])) {
-          files[field].push(file);
-        } else {
-          files[field] = [files[field], file];
-        }
-      } else {
-        files[field] = file;
-      }
-    });
-    if (opts.onFileBegin) {
-      form.on('fileBegin', opts.onFileBegin);
+    }else{
+      form = opts;
     }
+    
     form.parse(ctx.req);
   });
 }
